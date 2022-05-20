6b4c6bcd492af774fb85f8fc5e76e3ca99132a92
palblo <100297632+palblo@users.noreply.github.com>
2022-05-20 21:12:25 +0200

    Update Spammer.py

--- WebhookSpammer/Spammer.py
+++ WebhookSpammer/Spammer.py
@@ -1,26 +1,41 @@
+from pystyle import Colorate, Colors, Write      
+import requests                                  
+from threading import Thread                     
 from time import sleep
-from colorama import Fore
-from pystyle import Colorate, Colors, Write
-from discord import Webhook, RequestsWebhookAdapter
-import threading, requests
 
-print(Colorate.Vertical(Colors.purple_to_red, '''
+print(Colorate.Horizontal(Colors.purple_to_red, '''
 
-╦ ╦╔═╗╔╗ ╦ ╦╔═╗╔═╗╦╔═  ┌─┐┌─┐┌─┐┌┬┐┌┬┐┌─┐┬─┐
-║║║║╣ ╠╩╗╠═╣║ ║║ ║╠╩╗  └─┐├─┘├─┤││││││├┤ ├┬┘
-╚╩╝╚═╝╚═╝╩ ╩╚═╝╚═╝╩ ╩  └─┘┴  ┴ ┴┴ ┴┴ ┴└─┘┴└─ By Pąblo#4316
-Pąblo#4316 | https://github.com/palblo/DiscordWebhookSpammer
+┬ ┬┌─┐┌┐ ┬ ┬┌─┐┌─┐┬┌─  ┌─┐┌─┐┌─┐┌┬┐┌┬┐┌─┐┬─┐ 
+│││├┤ ├┴┐├─┤│ ││ │├┴┐  └─┐├─┘├─┤││││││├┤ ├┬┘
+└┴┘└─┘└─┘┴ ┴└─┘└─┘┴ ┴  └─┘┴  ┴ ┴┴ ┴┴ ┴└─┘┴└─ By Pąblo#4316
+
+Pąblo#4316 | https://github.com/palblo/SpotifyGen
+'''))
+9*
+print(Colorate.Horizontal(Colors.red_to_white, '''
+VERSION: V1.2
+NOTE : the message counter may not be accurate
+
 '''))
 
-url = Write.Input(f"link -> ", Colors.red_to_purple, interval=0.00005)
+url = Write.Input(f"\nlink -> ", Colors.red_to_purple, interval=0.00005)
 message = Write.Input(f"message -> ", Colors.red_to_purple, interval=0.00005)
 print()
 
+data = {
+    "content": message,
+    "username": "SPAMMER_V1.2"
+}
+
+count = 0
+
 def WebHook():
-    webhook = Webhook.from_url(url, adapter=RequestsWebhookAdapter())
-    webhook.send(message)
-    print(Colorate.Horizontal(Colors.red_to_purple, f"Sent MSG", 1))
+    global count
+    r = requests.post(url, data=data)
+    if r.status_code == 204:
+        messagescount = (Colorate.Horizontal(Colors.red_to_purple, f"Messages : {count}", 1))
+        messages = print(messagescount, end='\r')
+        count += 1
 
 while True:
-    threading.Thread(target=WebHook).start()
-
+    Thread(target=WebHook).start()
